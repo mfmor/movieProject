b0f00f3fa18c9febce5cb27d95c6a9076dd16856
Fatih Mor <55854069+mfmor@users.noreply.github.com>
2022-12-16 23:31:59 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -3,14 +3,7 @@ class UI {
 
   }
   static addFromToUI(film) {
-    /*
-      <!-- <tr>
-      <td><img src="" class="img-fluid img-thumbnail"></td>
-      <td></td>
-      <td></td>
-      <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-      </tr> -->
-    */
+   
 
     const filmListGroup = document.getElementById("films")
 
